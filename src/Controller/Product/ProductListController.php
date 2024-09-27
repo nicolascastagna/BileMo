188d811f81f383b4f9dbbbae826094cfe6db82cf
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2024-09-27 11:26:38 +0200

    add cache in ProductList

--- src/Controller/Product/ProductListController.php
+++ src/Controller/Product/ProductListController.php
@@ -10,18 +10,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductListController extends AbstractController
 {
     #[Route('/products', name: 'api_products', methods: [Request::METHOD_GET])]
-    public function list(Request $request, ProductListServiceInterface $productListService): JsonResponse
-    {
-        $result = $productListService->getPaginatedProducts($request);
+    public function list(
+        Request $request,
+        ProductListServiceInterface $productListService,
+        TagAwareCacheInterface $cache
+    ): JsonResponse {
+        $cacheKey = 'product_list';
+        $cacheTag = 'product_data';
 
-        return new JsonResponse([
-            'status' => Response::HTTP_OK,
-            'data' => $result['data'],
-            'pagination' => $result['pagination']
-        ], Response::HTTP_OK);
+        $response = $cache->get($cacheKey, function (ItemInterface $item) use ($request, $productListService, $cacheTag) {
+            $item->expiresAfter(3600);
+            $item->tag($cacheTag);
+
+            $result = $productListService->getPaginatedProducts($request);
+
+            return new JsonResponse([
+                'status' => Response::HTTP_OK,
+                'data' => $result['data'],
+                'pagination' => $result['pagination']
+            ], Response::HTTP_OK);
+        });
+
+        return $response;
     }
 }
